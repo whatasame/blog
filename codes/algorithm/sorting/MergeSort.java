297a986560f68b333ce6820b00a476e458939609
whatasame <whatasame@gmail.com>
2023-01-05 22:16:33 +0900

    Feat: Merge sort

---
+++ codes/algorithm/sorting/MergeSort.java
@@ -0,0 +1,51 @@
+package algorithm.sorting;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public void sort(int[] data, int left, int right)
+    {
+        if (left < right) {
+            int middle = left + ((right - left) / 2); // Half index of data
+
+            sort(data, left, middle); // Sort left subarray
+            sort(data, middle + 1, right); // Sort right subarray
+
+            merge(data, left, middle, right); // Merge subarray
+        }
+    }
+
+    public void merge(int[] data, int left, int mid, int right) {
+
+        // Divide original data into two subarray
+        int[] leftArray = Arrays.copyOfRange(data, left, mid + 1);
+        int[] rightArray = Arrays.copyOfRange(data, mid + 1, right + 1);
+
+        // Compare two subarray and merge
+        int i = left; // merged array index
+        int l = 0, r = 0; // left, right subarray index
+        for (; l < leftArray.length && r < rightArray.length; i++) {
+            if (leftArray[l] < rightArray[r]) {
+                data[i] = leftArray[l];
+                l++;
+            } else {
+                data[i] = rightArray[r];
+                r++;
+            }
+        }
+
+        // Merge remain data after compare
+        while (l < leftArray.length) {
+            data[i] = leftArray[l];
+            l++;
+            i++;
+        }
+        while (r < rightArray.length) {
+            data[i] = rightArray[r];
+            r++;
+            i++;
+        }
+
+    }
+}

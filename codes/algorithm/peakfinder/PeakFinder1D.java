a529324bf6bdf8368fea316a4f15d44c8438e058
whatasame <whatasame@gmail.com>
2023-01-06 18:18:50 +0900

    Feat: Peak finder in one dimension

---
+++ codes/algorithm/peakfinder/PeakFinder1D.java
@@ -0,0 +1,44 @@
+package algorithm.peakfinder;
+
+import java.util.Arrays;
+
+public class PeakFinder1D {
+
+    public static int find(int[] data) {
+
+        int target;
+        int left = 0, right = data.length - 1;
+
+        while (left < right) {
+            target = left + (right - left) / 2;
+
+            if (data[target] < data[target + 1]) {
+                left = target + 1;
+            } else{
+                right = target;
+            }
+        }
+
+        return left;
+    }
+
+    public static int[] getRandomData(int count) {
+        int[] data = new int[count];
+
+        for (int i = 0; i < data.length; i++) {
+            data[i] = (int) (Math.random() * 10 + 1);
+        }
+
+        return data;
+    }
+
+    public static void main(String[] args) {
+        int[] data = getRandomData(20);
+
+        int index = find(data);
+
+        System.out.println(Arrays.toString(data));
+        System.out.printf("Peak is %d (index: %d)", data[index], index);
+    }
+
+}

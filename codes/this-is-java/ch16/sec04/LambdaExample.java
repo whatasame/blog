ad0e9e666402170cfef428ea3df680ed014b538e
whatasame <whatasame@gmail.com>
2023-02-02 16:14:06 +0900

    Feat: Lambda with return

---
+++ codes/this-is-java/ch16/sec04/LambdaExample.java
@@ -0,0 +1,20 @@
+package ch16.sec04;
+
+public class LambdaExample {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        person.action((x, y) -> {
+            double result = x + y;
+            return result;
+        });
+
+        person.action((x, y) -> (x + y));
+
+        person.action((x, y) -> sum(x, y));
+    }
+
+    public static double sum(double x, double y) {
+        return x + y;
+    }
+}

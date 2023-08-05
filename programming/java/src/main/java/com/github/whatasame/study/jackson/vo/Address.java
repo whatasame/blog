5f1d9dbc7de429aee7d4d5bf27fc9cf4d1018aa5
whatasame <whatasame@gmail.com>
2023-08-05 17:48:41 +0900

    test: Jackson 직렬화/역직렬화 학습 테스트

---
+++ programming/java/src/main/java/com/github/whatasame/study/jackson/vo/Address.java
@@ -0,0 +1,20 @@
+package com.github.whatasame.study.jackson.vo;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public class Address {
+
+    private final String address;
+
+    /**
+     * 인스턴스 필드가 하나인 경우 @JsonCreator를 반드시 달아야한다.
+     */
+    @JsonCreator
+    public Address(final String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+}

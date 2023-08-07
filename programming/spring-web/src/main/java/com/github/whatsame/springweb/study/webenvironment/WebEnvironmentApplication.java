7923131828e64fb26099645600543f72473ae254
whatasame <whatasame@gmail.com>
2023-08-07 22:10:56 +0900

    test: @SpringBootTeset webEnvironment 학습 테스트

---
+++ programming/spring-web/src/main/java/com/github/whatsame/springweb/study/webenvironment/WebEnvironmentApplication.java
@@ -0,0 +1,13 @@
+package com.github.whatsame.springweb.study.webenvironment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEnvironmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebEnvironmentApplication.class, args);
+
+    }
+}
---
+++ programming/spring-web/src/test/java/com/github/whatsame/springweb/study/webenvironment/WebEnvironmentDefinedPortTest.java
@@ -0,0 +1,40 @@
+package com.github.whatsame.springweb.study.webenvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+class WebEnvironmentDefinedPortTest {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    Environment environment;
+
+    @Test
+    @DisplayName("WebEnvironment 값이 DEFINED_PORT일 때 "
+        + "ServletWebServerApplicationContext를 생성하고 "
+        + "지정한 포트 번호를 갖는 내장 Servlet 톰캣 서버를 띄운다.")
+    void definedPortTest() {
+        /* given */
+
+
+        /* when */
+
+
+        /* then */
+        assertThat(applicationContext)
+            .isInstanceOf(ServletWebServerApplicationContext.class); // Embedded Servlet web server
+        assertThat(environment.getProperty("local.server.port"))
+            .isEqualTo("8080"); // Defined port number
+    }
+}

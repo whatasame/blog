7363c5f8c41d5f6988568731ac7f77d3961c41d2
whatasame <97666463+whatasame@users.noreply.github.com>
2023-09-04 21:23:49 +0900

    build: MySQL을 사용하는 Spring Data JPA 모듈 생성

---
+++ spring-data-jpa-mysql/src/main/java/com/github/whatasame/springdatajpamysql/SpringDataJpaMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.github.whatasame.springdatajpamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaMysqlApplication.class, args);
+    }
+
+}

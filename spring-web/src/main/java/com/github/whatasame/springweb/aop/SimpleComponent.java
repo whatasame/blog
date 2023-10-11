2ed310da1933454b891e40f3823d8796b587912c
whatasame <97666463+whatasame@users.noreply.github.com>
2023-10-11 21:20:29 +0900

    test: AOP proxy 객체 학습 테스트

---
+++ spring-web/src/main/java/com/github/whatasame/springweb/aop/SimpleComponent.java
@@ -0,0 +1,8 @@
+package com.github.whatasame.springweb.aop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleComponent {
+
+}
--- spring-web/src/test/java/com/github/whatasame/springweb/aop/AopTest.java
+++ spring-web/src/test/java/com/github/whatasame/springweb/aop/AopTest.java
@@ -1,5 +1,7 @@
 package com.github.whatasame.springweb.aop;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -8,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -18,6 +21,9 @@ class AopTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    ApplicationContext applicationContext;
 
     @Test
     @DisplayName("GET 요청을 보내 로그가 정상적으로 찍히는지 확인한다.")
@@ -33,5 +39,35 @@ class AopTest {
                 .content("{\"greeting\": \"안녕하세요\"}")
             )
             .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("AOP 타겟 컴포넌트는 Proxy 객체로 생성된다.")
+    void proxyComponent() throws Exception {
+        /* given */
+
+        /* when */
+        final CokeController cokeController = applicationContext.getBean(CokeController.class);
+
+        /* then */
+        assertAll(
+            () -> assertThat(cokeController).isInstanceOf(CokeController.class),
+            () -> assertThat(cokeController).isNotExactlyInstanceOf(CokeController.class)
+        );
+    }
+
+    @Test
+    @DisplayName("일반 컴포넌트는 Proxy 객체로 생성되지 않는다.")
+    void normalComponent() throws Exception {
+        /* given */
+
+        /* when */
+        final SimpleComponent simpleComponent = applicationContext.getBean(SimpleComponent.class);
+
+        /* then */
+        assertAll(
+            () -> assertThat(simpleComponent).isInstanceOf(SimpleComponent.class),
+            () -> assertThat(simpleComponent).isExactlyInstanceOf(SimpleComponent.class)
+        );
     }
 }
